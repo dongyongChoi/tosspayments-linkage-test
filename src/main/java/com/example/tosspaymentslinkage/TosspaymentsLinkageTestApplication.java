68962a457412a9f86ea405feb7af8c7eabca537d
dongyongChoi <cni274@gmail.com>
2023-04-08 12:40:23 +0900

    토스페이먼츠 결제창 연동 테스트 프로젝트 시작

---
+++ src/main/java/com/example/tosspaymentslinkage/TosspaymentsLinkageTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tosspaymentslinkage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosspaymentsLinkageTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TosspaymentsLinkageTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tosspaymentslinkage/TosspaymentsLinkageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tosspaymentslinkage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosspaymentsLinkageTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
